51ebd77ae249505d90091e5307e46fe7ddcd1c5a
Joshua Tucker <thejltucker@gmail.com>
2019-12-22 17:17:26 -0600

    Add project files.

---
+++ VoteManagerAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace VoteManagerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ VoteManagerAPI/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(VoteManagerAPI.Startup))]
+
+namespace VoteManagerAPI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
