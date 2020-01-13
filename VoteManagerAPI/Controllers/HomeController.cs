407d4b32a59bb97d11191d67dc247a06eafd7dd6
Joshua Tucker <the1jtux@gmail.com>
2020-01-12 21:07:49 -0500

    Changes to site flow

--- VoteManagerAPI/Controllers/HomeController.cs
+++ VoteManagerAPI/Controllers/HomeController.cs
@@ -8,11 +8,6 @@ namespace VoteManagerAPI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            ViewBag.Title = "Home Page";
-
-            return View();
-        }
+        public ActionResult Index() => RedirectToAction("Index", "Help");
     }
 }
